5a2ede6ccc692309d13a3f605ba5338f7bb533ad
Dinesh726544 <bobi726544@gmail.com>
2024-06-13 15:16:36 +0530

    Revert "second commit"
    This reverts commit f099a9e3e5a9fbe2317885744b0e45e89d885682.

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,7 @@ const app = express();
 
 // Middleware for handling CORS POLICY
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin : "*"
 }));
 
 // Middleware for parsing request body
