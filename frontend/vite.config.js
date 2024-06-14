d72d85b1e40355d8f33b959d2e22c8307fb71f46
Dinesh726544 <bobi726544@gmail.com>
2024-06-14 06:45:21 +0530

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      "/api" : "http://book-store-inky-sigma.vercel.app"
+      "/api" : "http://book-store-seven-kappa.vercel.app"
     }
   },
   plugins: [react()],
