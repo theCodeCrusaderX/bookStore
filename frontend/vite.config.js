b331117c9543636179fed7f660728edc13cea048
Dinesh726544 <bobi726544@gmail.com>
2024-06-14 06:55:21 +0530

    g

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      "/api" : "http://book-store-seven-kappa.vercel.app"
+      "/api" : "https://book-store-seven-kappa.vercel.app/api/"
     }
   },
   plugins: [react()],
