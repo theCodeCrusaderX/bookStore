d591c06a836dfa4a90e6d62dd9cde84e8709adf2
thecodecrusader <bobi726544@gmail.com>
2025-04-11 13:16:41 +0530

    add railway msql service insted of local

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,19 @@
 export const PORT = 5555;
 
-
-
 import { Sequelize } from 'sequelize';
+import 'dotenv/config'
 
-export const sequelize = new Sequelize('bookStoreApp', 'root', 'Admin@1234', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 export const connectDB = async () => {
   try {
