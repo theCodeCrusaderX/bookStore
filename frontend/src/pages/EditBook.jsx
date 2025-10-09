30705617987306054d42530a7db6c9994c656110
theCodeCrusaderX <bobi726544@gmail.com>
2025-10-09 16:17:57 +0530

    fix ui of edit book

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -61,7 +61,7 @@ const EditBook = () => {
       <BackButton />
       <h1 className='text-3xl my-4'>Edit Book</h1>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[350px] md:w-[600px] p-4 mx-auto '>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
@@ -95,7 +95,7 @@ const EditBook = () => {
             type='text'
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-500 px-4 py-2 w-full h-32 resize-none'
           />
         </div>
         <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
